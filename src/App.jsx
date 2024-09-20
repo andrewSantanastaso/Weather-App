af34325ab4a4edf30d588f686f7e3a8b7c2c23d2
andrewSantanastaso <andrewsantanastaso@gmail.com>
2024-09-19 20:36:57 -0400

    weather app complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import * as weatherService from "./services/weatherService";
+import WeatherSearch from "./components/WeatherSearch";
+import WeatherDetails from "./components/WeatherDetails";
+import { useState } from "react";
+
 const App = () => {
-  return <h1>Hello World</h1>;
+  const [weather, setWeather] = useState({});
+  const fetchData = async (city) => {
+    const data = await weatherService.show(city);
+    const newWeatherState = {
+      location: data.location.name,
+      temperature: data.current.temp_f,
+      condition: data.current.condition.text,
+    };
+    setWeather(newWeatherState);
+  };
+
+  return (
+    <main>
+      <h1>Weather APi</h1>
+      <WeatherSearch fetchData={fetchData} />
+      <WeatherDetails weather={weather} />
+    </main>
+  );
 };
 
 export default App;
